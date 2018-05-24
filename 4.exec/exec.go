1293096bef2c14890b055eb1af435e2ee5865abd
wangzhaoxiang <wangzhaoxiang@huania.com>
2018-05-24 17:01:22 +0800

    exec

--- 4.exec/exec.go
+++ 4.exec/exec.go
@@ -2,24 +2,44 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 	"os/exec"
 )
 
 func main() {
-	cmd := exec.Command("free")
-	// deepin指令
-	// free
-	// name	args
-	// ifconfig
-	// ls	-a
-	// ls
-	// cat	/etc/deepin-version
-	// uname -a
-	// man	chmod
-	// locale
-	out, err := cmd.Output()
+	test2()
+}
+func test1() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		cmd := exec.Command("git")
+		// deepin指令
+		// google-chrome	谷歌
+		// free
+		// name	args
+		// ifconfig
+		// ls	-a
+		// ls
+		// cat	/etc/deepin-version
+		// uname -a
+		// man	chmod
+		// locale
+		out, err := cmd.Output()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(out))
+
+	})
+	http.ListenAndServe(":8080", nil)
+}
+func test2() {
+	cmd := exec.Command("ping", "baidu.com")
+	cmd.Stdout = os.Stdout
+	/* file, err := os.Open("4.exec/command.txt")
 	if err != nil {
-		fmt.Println(err)
+		os.Exit(1)
 	}
-	fmt.Println(string(out))
+	cmd.Stdin = file */
+	cmd.Run()
 }
